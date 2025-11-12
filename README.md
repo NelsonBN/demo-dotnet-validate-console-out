cae608176e4b03c85903beba78d205d9ca29aa6a
Nelson Nobre <technobre@gmail.com>
2025-11-12 00:03:05 +0000

    Kickoff

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demo - How can validate the output Console in .NET
+
+This is a simple demo project that shows how to validate the output of the Console in a .NET application. It demonstrates how to capture. This can be useful for testing purposes, ensuring that the console output is as expected.
---
+++ src/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+var stringBuilder = new StringBuilder();
+var stringWriter = new StringWriter(stringBuilder);
+
+var originalOutput = Console.Out;
+var originalError = Console.Error;
+
+Console.SetOut(stringWriter);
+Console.SetError(stringWriter);
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine();
+Console.Error.WriteLine("This is an error message.");
+
+
+var lines = stringBuilder
+    .ToString()
+    .Split(Environment.NewLine);
+
+if (lines[0] != "Hello, World!") throw new Exception("First line does not match expected output.");
+if (lines[1] != "") throw new Exception("Second line is not empty as expected.");
+if (lines[2] != "This is an error message.") throw new Exception("Third line does not match expected error message.");
+if (lines[3] != "") throw new Exception("Fourth line is not empty as expected.");
+
+
+Console.SetOut(originalOutput);
+Console.SetError(originalError);
+
+
+Console.WriteLine("All assertions passed.");
